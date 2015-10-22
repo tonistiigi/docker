496e63f37f138bbbff1ace468bf14e3bc121908e
Tonis Tiigi <tonistiigi@gmail.com>
2015-10-22 12:20:30 -0700

    Change invalid tag error message
    Signed-off-by: Tonis Tiigi <tonistiigi@gmail.com>

--- integration-cli/docker_cli_build_test.go
+++ integration-cli/docker_cli_build_test.go
@@ -4417,7 +4417,7 @@ func (s *DockerSuite) TestBuildInvalidTag(c *check.C) {
 	_, out, err := buildImageWithOut(name, "FROM scratch\nMAINTAINER quux\n", true)
 	// if the error doesnt check for illegal tag name, or the image is built
 	// then this should fail
-	if !strings.Contains(out, "Illegal tag name") || strings.Contains(out, "Sending build context to Docker daemon") {
+	if !strings.Contains(out, "invalid reference format") || strings.Contains(out, "Sending build context to Docker daemon") {
 		c.Fatalf("failed to stop before building. Error: %s, Output: %s", err, out)
 	}
 }
