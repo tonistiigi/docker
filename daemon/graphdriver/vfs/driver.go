82bf291b64126307431fb71b4614a93da4a1e780
Derek McGowan <derek@mcgstyle.net>
2015-11-02 14:01:51 -0800

    Add layer store
    Layer store manages read-only and read-write layers on a union file system.
    Read only layers are always referenced by content addresses.
    Read-write layer identifiers are handled by the caller but upon registering
    its difference, the committed read-only layer will be referenced by content
    hash.
    
    Signed-off-by: Derek McGowan <derek@mcgstyle.net> (github: dmcgowan)

--- daemon/graphdriver/vfs/driver.go
+++ daemon/graphdriver/vfs/driver.go
@@ -1,4 +1,4 @@
-// +build daemon
+// +build linux windows
 
 package vfs
 
@@ -12,6 +12,11 @@ import (
 	"github.com/docker/docker/pkg/idtools"
 
 	"github.com/opencontainers/runc/libcontainer/label"
+)
+
+var (
+	// CopyWithTar defines the copy method to use.
+	CopyWithTar = chrootarchive.CopyWithTar
 )
 
 func init() {
@@ -89,7 +94,7 @@ func (d *Driver) Create(id, parent, mountLabel string) error {
 	if err != nil {
 		return fmt.Errorf("%s: %s", parent, err)
 	}
-	if err := chrootarchive.CopyWithTar(parentDir, dir); err != nil {
+	if err := CopyWithTar(parentDir, dir); err != nil {
 		return err
 	}
 	return nil
